a0f8e52fd868dfb452931f8adeb5185909bc4719
Jazzberries <anime.flick.chick@gmail.com>
2022-04-18 11:33:26 +1000

    Added switch cases for license badges

--- readme4.md
+++ readme4.md
@@ -1,16 +1,16 @@
-# 456
-  # 456
-  # 456
-  # 456
-  # 456
-  # 456
+# 345345345
+  # 345345
+  # 435345
+  # 435345
+  # 345
+  # 345
   # MIT
-  # h
-  # h
+  # g
+  # g
   # [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg?style=for-the-badge)](https://choosealicense.com/licenses/mit/)
-  License Badge
-  
-  #undefined
-  #undefined
+    License Badge
+
+
+
 
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -20,16 +20,45 @@ function renderLicense(license) {
       licenseBadge = "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg?style=for-the-badge)";
       licenseLink = "https://choosealicense.com/licenses/mit/";
       break;
+
+    case "GNU GPLv3":
+      licenseBadge = "![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg?style=for-the-badge)";
+      licenseLink = "https://choosealicense.com/licenses/gpl-3.0/";
+      break;
+
+    case "The Unlicense":
+      licenseBadge = "![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg?style=for-the-badge)";
+      licenseLink = "https://choosealicense.com/licenses/unlicense/";
+      break;
+
+    case "Apache License 2.0":
+      licenseBadge = "![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg?style=for-the-badge)";
+      licenseLink = "https://choosealicense.com/licenses/apache-2.0/";
+      break;
+
+    case "Mozilla Public License 2.0":
+      licenseBadge = "![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg?)";
+      licenseLink = "https://choosealicense.com/licenses/mpl-2.0/";
+      break;
+
+    case "None":
+      licenseBadge = "";
+      licenseLink = "";
+      break;
+
+    default:
+      return "";
+
   }
-  return (`[${licenseBadge}](${licenseLink})`)
-    `License Badge`
+  return `[${licenseBadge}](${licenseLink})
+    License Badge`
 
 }
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown({ title, description, installation, usage, contribution, test, license, github, email, createLicenseBadge, licenseBadge, licenseLink }) {
+function generateMarkdown({ title, description, installation, usage, contribution, test, license, github, email, createLicenseBadge }) {
   // renderLicense();
-  console.log(licenseBadge, "Success!");
+  console.log(renderLicense.licenseLink, "Success!");
   return `# ${title}
   # ${description}
   # ${installation}
@@ -40,8 +69,9 @@ function generateMarkdown({ title, description, installation, usage, contributio
   # ${github}
   # ${email}
   # ${createLicenseBadge}
-  #${licenseBadge}
-  #${licenseLink}
+
+
+
 
 
 `;
